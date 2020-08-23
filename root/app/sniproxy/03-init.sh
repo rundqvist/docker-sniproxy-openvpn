e84aef1468279dd01294d37bb9a2437abb92ff12
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-08-23 10:14:59 +0200

    Updated base image

--- root/app/sniproxy/03-init.sh
+++ root/app/sniproxy/03-init.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
 
-mkdir -p /etc/sniproxy
-cp -f /app/sniproxy/sniproxy.conf /etc/sniproxy/sniproxy.conf
-
-echo "" >> /etc/supervisord.conf
-cat /app/sniproxy/supervisord.conf >> /etc/supervisord.conf
