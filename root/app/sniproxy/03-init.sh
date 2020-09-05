025f8e396e07cb1b7a892455acaa4108464fa9dd
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-05 13:15:39 +0200

    Implemented new logging

--- root/app/sniproxy/03-init.sh
+++ root/app/sniproxy/03-init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if expr "${NETWORK}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.0$' >/dev/null; then
-    log -i "Adding route for communication with network $NETWORK/24";
+    log -i sniproxy "Adding route for communication with network $NETWORK/24";
     route add -net ${NETWORK} netmask 255.255.255.0 gw $(ip route | awk '/default/ { print $3 }')
 else
-    log -w "NETWORK missing or wrong format. Add this if you are experiencing communication problems.";
+    log -w sniproxy "NETWORK missing or wrong format. May cause communication problems.";
 fi
