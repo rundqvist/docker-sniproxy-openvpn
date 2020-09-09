18a2b5a301fdceb805d3e35d86d18b8e106aad61
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-09 18:18:54 +0200

    Moved route adding to base container

--- root/app/sniproxy/03-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if expr "$(var NETWORK)" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.0$' >/dev/null; then
-    log -i sniproxy "Adding route for communication with network $(var NETWORK)/24";
-    route add -net $(var NETWORK) netmask 255.255.255.0 gw $(ip route | awk '/default/ { print $3 }')
-else
-    log -w sniproxy "NETWORK missing or wrong format. May cause communication problems.";
-fi
