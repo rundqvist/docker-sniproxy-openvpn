a470fd1b72bb81f64b43eba19eaa6210daba8d95
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-10-05 18:14:39 +0200

    Updated base image
    Fixes #4

--- root/app/dnsmasq/healthcheck.sh
+++ root/app/dnsmasq/healthcheck.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if [ "$(var DNS_ENABLED)" = "true" ] ; then
-    if [ "$(ps | grep "dnsmasq" | wc -l)" -gt 1 ] ; then
-        log -d dnsmasq "Dnsmasq is running."
+if [ "$(var DNS_ENABLED)" = "true" ]
+then
+    if [ "$(ps | grep "dnsmasq" | wc -l)" -gt 1 ]
+    then
+        log -d "Dnsmasq is running."
     else
-        log -e dnsmasq "Dnsmasq is not running."
+        log -e "Dnsmasq is not running."
         exit 1;
     fi
 fi
